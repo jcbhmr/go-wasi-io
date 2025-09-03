79e4e70ec1eba930816acec2e877bbbb0f86bb49
Jacob Hummer <jcbhmr@outlook.com>
2025-09-03 04:30:49 +0000

    save

--- error/error.wit.go
+++ error/error.wit.go
@@ -2,14 +2,14 @@
 
 //go:build wasip2
 
-// Package ioerror represents the imported interface "wasi:io/error@0.2.2".
+// Package ioerror represents the imported interface "wasi:io/error@0.2.3".
 package ioerror
 
 import (
 	"go.bytecodealliance.org/cm"
 )
 
-// Error represents the imported resource "wasi:io/error@0.2.2#error".
+// Error represents the imported resource "wasi:io/error@0.2.3#error".
 //
 // A resource which represents some error information.
 //
